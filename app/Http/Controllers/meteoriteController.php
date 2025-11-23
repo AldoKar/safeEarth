b40dbc10a7eab465020179d2da22fd40d327c4ee
Roberto0611 <ochoacuevasr@gmail.com>
2025-11-23 12:48:00 -0600

    arreglo ayuda

--- app/Http/Controllers/meteoriteController.php
+++ app/Http/Controllers/meteoriteController.php
@@ -12,7 +12,7 @@ class meteoriteController extends Controller
     {
         $apiKey = env('NASA_API_KEY', 'DEMO_KEY');
 
-        $response = Http::get('https://api.nasa.gov/neo/rest/v1/neo/2035396?api_key=', [
+        $response = Http::get('https://api.nasa.gov/neo/rest/v1/neo/2035396', [
             'api_key' => $apiKey
         ]);
 
@@ -21,7 +21,43 @@ class meteoriteController extends Controller
             return response()->json($data);
         }
 
-        return response()->json(['error' => 'API request failed']);
+        // Return consistent structure on error to prevent frontend crashes
+        return response()->json([
+            'name' => 'Unknown',
+            'id' => '0',
+            'neo_reference_id' => '0',
+            'absolute_magnitude_h' => 0,
+            'is_potentially_hazardous_asteroid' => false,
+            'estimated_diameter' => [
+                'kilometers' => [
+                    'estimated_diameter_min' => 0,
+                    'estimated_diameter_max' => 0
+                ],
+                'meters' => [
+                    'estimated_diameter_min' => 0,
+                    'estimated_diameter_max' => 0
+                ],
+                'miles' => [
+                    'estimated_diameter_min' => 0,
+                    'estimated_diameter_max' => 0
+                ]
+            ],
+            'close_approach_data' => [],
+            'orbital_data' => [
+                'eccentricity' => '0',
+                'semi_major_axis' => '0',
+                'inclination' => '0',
+                'orbital_period' => '0',
+                'orbit_class' => [
+                    'orbit_class_type' => 'Unknown',
+                    'orbit_class_description' => 'Data unavailable'
+                ],
+                'first_observation_date' => date('Y-m-d'),
+                'last_observation_date' => date('Y-m-d')
+            ],
+            'is_sentry_object' => false,
+            'error' => 'API request failed'
+        ], 503);
     }
 
     public function getKepplerData()
